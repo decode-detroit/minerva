f9cc59f02b8974b02278edf042bc34df5b6bbac1
Patton Doyle <patton@decodedetroit.com>
2021-04-16 00:25:53 -0400

    Partial Progress for Websocket Implementation

--- src/web_interface/mod.rs
+++ src/web_interface/mod.rs
@@ -27,9 +27,20 @@ use std::path::PathBuf;
 use std::time::Duration;
 use std::num::ParseIntError;
 
-// Import tokio and warp modules
+// Import Chrono features
+use chrono::NaiveDateTime;
+
+// Import Tokio and warp features
 use tokio::sync::{mpsc, oneshot};
 use warp::{http, Filter};
+use warp::ws::{WebSocket, Message};
+
+// Import stream-related features
+use async_stream::stream;
+use futures_util::{StreamExt, TryStreamExt};
+
+// Import serde feaures
+use serde::de::DeserializeOwned;
 
 /// Helper data types to formalize request structure
 #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
@@ -47,7 +58,7 @@ struct BroadcastEvent {
 }
 #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct ConfigFile {
+struct ConfigFile { 
     filename: String,
 }
 #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
@@ -71,6 +82,13 @@ struct Edit {
 #[serde(rename_all = "camelCase")]
 struct ErrorLog {
     filename: String,
+}
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct EventChange {
+    event_id: ItemId,
+    start_time: NaiveDateTime,
+    new_delay: Option<Duration>,
 }
 #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -169,10 +187,26 @@ impl From<DebugMode> for UserRequest {
         UserRequest::DebugMode(debug_mode.is_debug)
     }
 }
+impl From<Edit> for UserRequest {
+    fn from(edit: Edit) -> Self {
+        UserRequest::Edit {
+            modifications: edit.modifications,
+        }
+    }
+}
 impl From<ErrorLog> for UserRequest {
     fn from(error_log: ErrorLog) -> Self {
         UserRequest::ErrorLog {
             filepath: PathBuf::from(error_log.filename),
+        }
+    }
+}
+impl From<EventChange> for UserRequest {
+    fn from(event_change: EventChange) -> Self {
+        UserRequest::EventChange {
+            event_id: event_change.event_id,
+            start_time: event_change.start_time,
+            new_delay: event_change.new_delay,
         }
     }
 }
@@ -241,17 +275,35 @@ impl WebInterface {
     /// A method to listen for connections from the internet
     ///
     pub async fn run(&mut self) {
+        // Spin up a thread to manage web socket requests
+        tokio::spawn(|| {
+                                // Add the tx line to the listeners
+                                self.listeners.push(tx);
+
+                                // Indicate success
+                                Manager::handle_reply(&Ok(()), reply_channel);
+        })
+        
         // Create the index filter
         let readme = warp::get()
             .and(warp::path::end())
             .and(warp::fs::file("./index.html"));
+
+        // Create the wwebsocket filter
+        let listen = warp::path("listen")
+            .and(WebInterface::with_sender(self.web_send.clone()))
+            .and(warp::ws())
+            .map(|sender, ws: warp::ws::Ws| {
+                // This will call the function if the handshake succeeds.
+                ws.on_upgrade(move |socket| WebInterface::add_listener(sender, socket))
+            });
 
         // Create the all event change filter
         let all_event_change = warp::post()
             .and(warp::path("allEventChange"))
             .and(warp::path::end())
             .and(WebInterface::with_sender(self.web_send.clone()))
-            .and(WebInterface::with_all_event_change())
+            .and(WebInterface::with::<AllEventChange>())
             .and_then(WebInterface::handle_request);
 
         // Create the all stop filter
@@ -265,7 +317,7 @@ impl WebInterface {
         let broadcast_event = warp::path("broadcastEvent")
             .and(warp::path::end())
             .and(WebInterface::with_sender(self.web_send.clone()))
-            .and(WebInterface::with_broadcast_event())
+            .and(WebInterface::with::<BroadcastEvent>())
             .and_then(WebInterface::handle_request);
 
         // Create the clear queue filter
@@ -289,7 +341,7 @@ impl WebInterface {
             .and(warp::path("configFile"))
             .and(warp::path::end())
             .and(WebInterface::with_sender(self.web_send.clone()))
-            .and(WebInterface::with_config_file())
+            .and(WebInterface::with::<ConfigFile>())
             .and_then(WebInterface::handle_request);
 
         // Create the cue event filter
@@ -297,7 +349,7 @@ impl WebInterface {
             .and(warp::path("cueEvent"))
             .and(warp::path::end())
             .and(WebInterface::with_sender(self.web_send.clone()))
-            .and(WebInterface::with_cue_event())
+            .and(WebInterface::with::<CueEvent>())
             .and_then(WebInterface::handle_request);
 
             // Create the debug mode filter
@@ -305,7 +357,15 @@ impl WebInterface {
             .and(warp::path("debugMode"))
             .and(warp::path::end())
             .and(WebInterface::with_sender(self.web_send.clone()))
-            .and(WebInterface::with_debug_mode())
+            .and(WebInterface::with::<DebugMode>())
+            .and_then(WebInterface::handle_request);
+
+        // Create the edit filter
+        let edit = warp::post()
+            .and(warp::path("edit"))
+            .and(warp::path::end())
+            .and(WebInterface::with_sender(self.web_send.clone()))
+            .and(WebInterface::with::<Edit>())
             .and_then(WebInterface::handle_request);
 
         // Create the error log filter
@@ -313,7 +373,15 @@ impl WebInterface {
             .and(warp::path("errorLog"))
             .and(warp::path::end())
             .and(WebInterface::with_sender(self.web_send.clone()))
-            .and(WebInterface::with_error_log())
+            .and(WebInterface::with::<ErrorLog>())
+            .and_then(WebInterface::handle_request);
+
+        // Create the event change filter
+        let event_change = warp::post()
+            .and(warp::path("eventChange"))
+            .and(warp::path::end())
+            .and(WebInterface::with_sender(self.web_send.clone()))
+            .and(WebInterface::with::<EventChange>())
             .and_then(WebInterface::handle_request);
 
         // Create the config file filter
@@ -321,7 +389,7 @@ impl WebInterface {
             .and(warp::path("gameLog"))
             .and(warp::path::end())
             .and(WebInterface::with_sender(self.web_send.clone()))
-            .and(WebInterface::with_game_log())
+            .and(WebInterface::with::<ConfigFile>())
             .and_then(WebInterface::handle_request);
 
         // Create the item information filter
@@ -337,7 +405,7 @@ impl WebInterface {
             .and(warp::path("processEvent"))
             .and(warp::path::end())
             .and(WebInterface::with_sender(self.web_send.clone()))
-            .and(WebInterface::with_process_event())
+            .and(WebInterface::with::<ProcessEvent>())
             .and_then(WebInterface::handle_request);
 
         // Create the redraw filter
@@ -353,7 +421,7 @@ impl WebInterface {
             .and(warp::path("saveConfig"))
             .and(warp::path::end())
             .and(WebInterface::with_sender(self.web_send.clone()))
-            .and(WebInterface::with_save_config())
+            .and(WebInterface::with::<SaveConfig>())
             .and_then(WebInterface::handle_request);
 
         // Create the scene change filter
@@ -361,7 +429,7 @@ impl WebInterface {
             .and(warp::path("sceneChange"))
             .and(warp::path::end())
             .and(WebInterface::with_sender(self.web_send.clone()))
-            .and(WebInterface::with_scene_change())
+            .and(WebInterface::with::<SceneChange>())
             .and_then(WebInterface::handle_request);
 
         // Create the config file filter
@@ -369,7 +437,7 @@ impl WebInterface {
             .and(warp::path("statusChange"))
             .and(warp::path::end())
             .and(WebInterface::with_sender(self.web_send.clone()))
-            .and(WebInterface::with_status_change())
+            .and(WebInterface::with::<StatusChange>())
             .and_then(WebInterface::handle_request);
 
         // Combine the filters
@@ -382,9 +450,9 @@ impl WebInterface {
             .or(config_file)
             .or(cue_event)
             .or(debug_mode)
-            // FIXME .or(edit)
+            .or(edit)
             .or(error_log)
-            // FIXME .or(event_change)
+            .or(event_change)
             .or(game_log)
             .or(get_item)
             .or(process_event)
@@ -398,6 +466,7 @@ impl WebInterface {
     }
 
     /// A function to handle incoming requests
+    /// 
     async fn handle_request<R>(
         web_send: WebSend,
         request: R,
@@ -436,6 +505,7 @@ impl WebInterface {
     }
 
     /// A function to handle get item requests (processed by the index)
+    /// 
     async fn handle_get_item(
         index_access: IndexAccess,
         get_item: GetItem,
@@ -455,70 +525,52 @@ impl WebInterface {
         ));
     }
 
-    /// A function to extract an all event change from the body of the message
-    fn with_all_event_change(
-    ) -> impl Filter<Extract = (AllEventChange,), Error = warp::Rejection> + Clone {
-        // When accepting a body, we want a JSON body (reject huge payloads)
-        warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+    /// A function to add a new websocket listener
+    /// 
+    async fn add_listener(web_send: WebSend, socket: WebSocket) {
+        // Split the socket into a sender and receiver
+        let (ws_tx, mut ws_rx) = socket.split();
+
+        // Use an unbounded channel to handle buffering and flushing of messages
+        let (tx, mut rx) = mpsc::unbounded_channel();
+        let stream = stream! {
+            while let Some(item) = rx.recv().await {
+                yield item;
+            }
+        };
+
+        // Forward messages until the line is dropped
+        tokio::spawn(
+            // Forward received messages
+            stream.forward(ws_tx)
+        );
+        
+        // Send the tx line to the Manager
+        let (reply_tx, reply_rx) = oneshot::channel();
+        if let Err(_) = sender.send((Request::Listen { tx }, reply_tx)).await {
+            // Wait for the reply
+            match reply_rx.await {
+                Ok(result) => {
+                    if !result.is_success() {
+                        return;
+                    }
+                }
+            
+                // Exit on broken pipe
+                Err(_) => return,
+            }
+            
+            // Drop the connection on failure
+            return;
+        }
+
+        // Wait for the line to be dropped (ignore incoming messages)
+        while let Some(_) = ws_rx.next().await {}
     }
 
-    /// A function to extract a broadcast event from the body of the message
-    fn with_broadcast_event(
-    ) -> impl Filter<Extract = (BroadcastEvent,), Error = warp::Rejection> + Clone {
-        // When accepting a body, we want a JSON body (reject huge payloads)
-        warp::body::content_length_limit(1024 * 16).and(warp::body::json())
-    }
-
-    /// A function to extract a config file from the body of the message
-    fn with_config_file() -> impl Filter<Extract = (ConfigFile,), Error = warp::Rejection> + Clone {
-        // When accepting a body, we want a JSON body (reject huge payloads)
-        warp::body::content_length_limit(1024 * 16).and(warp::body::json())
-    }
-
-    /// A function to extract a cue event from the body of the message
-    fn with_cue_event() -> impl Filter<Extract = (CueEvent,), Error = warp::Rejection> + Clone {
-        // When accepting a body, we want a JSON body (reject huge payloads)
-        warp::body::content_length_limit(1024 * 16).and(warp::body::json())
-    }
-
-    /// A function to extract a debug mode from the body of the message
-    fn with_debug_mode() -> impl Filter<Extract = (DebugMode,), Error = warp::Rejection> + Clone {
-        // When accepting a body, we want a JSON body (reject huge payloads)
-        warp::body::content_length_limit(1024 * 16).and(warp::body::json())
-    }
-
-    /// A function to extract an error log from the body of the message
-    fn with_error_log() -> impl Filter<Extract = (ErrorLog,), Error = warp::Rejection> + Clone {
-        // When accepting a body, we want a JSON body (reject huge payloads)
-        warp::body::content_length_limit(1024 * 16).and(warp::body::json())
-    }
-
-    /// A function to extract a game log from the body of the message
-    fn with_game_log() -> impl Filter<Extract = (GameLog,), Error = warp::Rejection> + Clone {
-        // When accepting a body, we want a JSON body (reject huge payloads)
-        warp::body::content_length_limit(1024 * 16).and(warp::body::json())
-    }
-
-    /// A function to extract a process event from the body of the message
-    fn with_process_event() -> impl Filter<Extract = (ProcessEvent,), Error = warp::Rejection> + Clone {
-        // When accepting a body, we want a JSON body (reject huge payloads)
-        warp::body::content_length_limit(1024 * 16).and(warp::body::json())
-    }
-
-    /// A function to extract a save config from the body of the message
-    fn with_save_config() -> impl Filter<Extract = (SaveConfig,), Error = warp::Rejection> + Clone {
-        // When accepting a body, we want a JSON body (reject huge payloads)
-        warp::body::content_length_limit(1024 * 16).and(warp::body::json())
-    }
-
-    /// A function to extract a scene change from the body of the message
-    fn with_scene_change() -> impl Filter<Extract = (SceneChange,), Error = warp::Rejection> + Clone {
-        // When accepting a body, we want a JSON body (reject huge payloads)
-        warp::body::content_length_limit(1024 * 16).and(warp::body::json())
-    }
-
-    /// A function to extract a status change from the body of the message
-    fn with_status_change() -> impl Filter<Extract = (StatusChange,), Error = warp::Rejection> + Clone {
+    // A function to extract a helper type from the body of the message
+    fn with<T>() -> impl Filter<Extract = (T,), Error = warp::Rejection> + Clone
+    where T: Send + DeserializeOwned {
         // When accepting a body, we want a JSON body (reject huge payloads)
         warp::body::content_length_limit(1024 * 16).and(warp::body::json())
     }
@@ -530,14 +582,14 @@ impl WebInterface {
         warp::any().map(move || web_send.clone())
     }
 
-    // A function to add the index access  to the filter
+    // A function to add the index access to the filter
     fn with_index(
         index_access: IndexAccess,
     ) -> impl Filter<Extract = (IndexAccess,), Error = std::convert::Infallible> + Clone {
         warp::any().map(move || index_access.clone())
     }
 
-    /// A function to add a specific request type to the filter
+    // A function to add a specific request type to the filter
     fn with_request(
         request: UserRequest,
     ) -> impl Filter<Extract = (UserRequest,), Error = std::convert::Infallible> + Clone {
